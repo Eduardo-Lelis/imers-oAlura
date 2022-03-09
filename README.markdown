fbd6c8e3078029586866da06d09d9217df313c3d
Eduardo Lelis <eduardodelelis@gmail.com>
2022-03-08 22:39:37 -0300

    Simple calculator

---
+++ README.markdown
@@ -0,0 +1,5 @@
+# Calculadora de Médias
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/eduardo-lelis/pen/XWzLbrO](https://codepen.io/eduardo-lelis/pen/XWzLbrO).
+
+
---
+++ dist/script.js
@@ -0,0 +1,21 @@
+function calculoMedia() 
+{
+var valor = parseFloat(document.getElementById("valor").value);
+var valor2 = parseFloat(document.getElementById("valor2").value);
+var valor3 = parseFloat(document.getElementById("valor3").value);
+var valor4 = parseFloat(document.getElementById("valor4").value);
+  
+var media = (valor + valor2 + valor3 + valor4) / 4;
+  
+  if(media<7)
+    {
+      document.getElementById("resultado").innerHTML =
+        "<font color='red'> <b> Reprovado com média igual a " + media.toFixed(1) + "</br>";
+    }if (media>=7)
+    {
+      document.getElementById("resultado").innerHTML =
+       "<font color='blue'> <b> Aprovado com média igual a" + media.toFixed(1) + "</br>";
+    }  
+  
+ 
+}
